2e0687d90e0c32db211cbef3f46ff8d1da27ad0b
Tarun <saitaruna@testvagrant.com>
2021-06-25 09:33:58 +0530

    javadocs added

--- selenium/src/main/java/utilities/InitializeDriver.java
+++ selenium/src/main/java/utilities/InitializeDriver.java
@@ -6,6 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InitializeDriver {
 
+    /**
+     * This method is used to initialize the driver with the given url
+     * @param url unified resource locator
+     * @return driver instance
+     */
+
     public static WebDriver initializeDriver(String url){
         WebDriverManager.chromedriver().setup(); // chrome driver setup using webdriver manager
         WebDriver driver=new ChromeDriver();
