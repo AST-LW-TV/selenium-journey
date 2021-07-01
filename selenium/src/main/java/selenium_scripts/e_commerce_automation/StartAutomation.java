a38b4b13f66185e857aa0ba604393fbc120e3944
Tarun <saitaruna@testvagrant.com>
2021-07-01 16:36:52 +0530

    refactored code - 99%

--- selenium/src/main/java/selenium_scripts/e_commerce_automation/StartAutomation.java
+++ selenium/src/main/java/selenium_scripts/e_commerce_automation/StartAutomation.java
@@ -10,6 +10,21 @@ import java.util.Scanner;
 public class StartAutomation {
 
     public static void main(String args[]) {
+
+        // REFACTORED CODE
+        // Main method is StartAutomation class
+        // - inputing the browser in which the automation to be done
+        // - inputing the type of registered or not registered
+        // - pages package
+        //   - HomePage -> add to cart operation, coupling to LoginPage
+        //   - LoginPage -> Authentication
+        //   - CheckOutPage -> payment operation
+        // - utilities package
+        //   - details -> json file which has the customer info
+        //   - GetDetails -> fetches the required data for automation
+        //   - PageReference is the page factory
+        //   - Utility -> starts the desired browser
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the browser : chrome / firefox...");
         String browser = input.nextLine();
